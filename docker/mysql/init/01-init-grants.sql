bec68c963da6460a1e90596262f40ac7b8f77cdf
amdanibik <dnamdan@gmail.com>
2026-04-07 19:20:39 +0700

    fix db

--- docker/mysql/init/01-init-grants.sql
+++ docker/mysql/init/01-init-grants.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE IF NOT EXISTS restaurant_test;
 
+CREATE USER IF NOT EXISTS 'restaurant'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'password';
+ALTER USER 'restaurant'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'password';
 GRANT ALL PRIVILEGES ON restaurant_development.* TO 'restaurant'@'%';
 GRANT ALL PRIVILEGES ON restaurant_test.* TO 'restaurant'@'%';
 FLUSH PRIVILEGES;
